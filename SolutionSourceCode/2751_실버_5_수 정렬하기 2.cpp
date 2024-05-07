2e11d48d0c406f72a221fed258a0d51d3226e208
shyoon <shyoon>
2024-05-07 19:13:02 +0900

    2751번 솔루션 업로드

---
+++ SolutionSourceCode/2751_실버_5_수 정렬하기 2.cpp
@@ -0,0 +1,35 @@
+﻿#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+int main(int argc, char* argv[])
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	int totalNumberCount = 0;
+
+	std::cin >> totalNumberCount;
+
+	std::vector<int> inputVector = std::vector<int>();
+
+	for (int i = 0; i < totalNumberCount; ++i)
+	{
+		int temp = 0;
+		std::cin >> temp;
+		inputVector.push_back(temp);
+	}
+
+	std::sort(inputVector.begin(), inputVector.end());
+
+	for (int i = 0; i < totalNumberCount; ++i)
+	{
+		std::cout << inputVector[i] << '\n';
+	}
+
+
+	return 0;
+}
