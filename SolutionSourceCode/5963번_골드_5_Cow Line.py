338b5227124601735d0728eea52f98645632bc74
Upsilion-Sigma-Eta <48117593+Upsilion-Sigma-Eta@users.noreply.github.com>
2024-08-31 09:50:04 +0900

    + 5963번 솔루션 업로드

---
+++ SolutionSourceCode/5963번_골드_5_Cow Line.py
@@ -0,0 +1,43 @@
+import math
+import sys
+
+def Solution():
+    N, K = map(int, sys.stdin.readline().strip().split())
+
+    for _ in range(K):
+        problem_type = sys.stdin.readline().strip()
+
+        if (problem_type == "P"):
+            problem_input = int(sys.stdin.readline().strip())
+            number_arr = [_ for _ in range(1, N + 1)]
+            sorted_number_arr = sorted(number_arr)
+            result_number_arr = []
+
+            index = 0
+            # 0 기반 인덱스로 변경
+            target_index = problem_input - 1
+            for _ in range(N):
+                possible_penumeration_count = math.factorial(len(sorted_number_arr) - 1)
+                index = target_index // possible_penumeration_count
+                result_number_arr.append(sorted_number_arr.pop(index))
+                target_index %= possible_penumeration_count
+
+            sys.stdout.write(" ".join(map(str, result_number_arr)) + "\n")
+        elif (problem_type == "Q"):
+            problem_input = list(map(int, sys.stdin.readline().strip().split()))
+
+            number_arr = problem_input
+
+            index = 0
+            weight = math.factorial(N - 1)
+            sorted_number_arr = sorted(number_arr)
+            for number in range(N):
+                index += weight * sorted_number_arr.index(number_arr[number])
+                sorted_number_arr.remove(number_arr[number])
+
+                if number < N - 1:
+                    weight //= (N - number - 1)
+
+            sys.stdout.write(str(index + 1) + "\n")
+
+Solution()
