5343c58579dbb647ce252ebfcbc3dfe1281031cf
Upsilion-Sigma-Eta <48117593+Upsilion-Sigma-Eta@users.noreply.github.com>
2024-09-07 15:09:59 +0900

    + 2338번 솔루션 업로드

---
+++ SolutionSourceCode/2338번_브론즈_5_긴자리 계산.py
@@ -0,0 +1,12 @@
+import sys
+
+def Solution():
+    A = int(sys.stdin.readline().strip())
+    B = int(sys.stdin.readline().strip())
+
+    print(A + B)
+    print(A - B)
+    print(A * B)
+
+if __name__ == "__main__":
+    Solution()
