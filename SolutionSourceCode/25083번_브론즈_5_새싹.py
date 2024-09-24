17f8fdb4b265e314a3b4b1ae4def56a3b13806e9
Upsilion-Sigma-Eta <48117593+Upsilion-Sigma-Eta@users.noreply.github.com>
2024-09-24 13:39:21 +0900

    + 25083번 솔루션 업로드

---
+++ SolutionSourceCode/25083번_브론즈_5_새싹.py
@@ -0,0 +1,14 @@
+import sys
+
+def Solution():
+    sprout = '''         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |'''
+
+    sys.stdout.write(sprout)
+
+if __name__ == "__main__":
+    Solution()
