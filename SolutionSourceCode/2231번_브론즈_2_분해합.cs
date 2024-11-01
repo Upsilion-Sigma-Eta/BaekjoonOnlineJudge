5532e339a620ed7bcbbb20ca9bab03736ae8929f
Upsilion-Sigma-Eta <48117593+Upsilion-Sigma-Eta@users.noreply.github.com>
2024-11-01 21:15:59 +0900

    + 2231번 솔루션 업로드

---
+++ SolutionSourceCode/2231번_브론즈_2_분해합.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            for (int i = 0; i < N; ++i)
+            {
+                string number = i.ToString();
+
+                int generator = i;
+
+                foreach (char c in number)
+                {
+                    generator += int.Parse(c.ToString());
+                }
+
+                if (generator == N)
+                {
+                    result = i;
+                    break;
+                }
+            }
+            
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
