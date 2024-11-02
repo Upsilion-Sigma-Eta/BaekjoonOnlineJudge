df03ae56d05e3a89d03ee2a324b53f8ff09f1fcb
Upsilion-Sigma-Eta <48117593+Upsilion-Sigma-Eta@users.noreply.github.com>
2024-11-02 20:24:33 +0900

    + 2292번 솔루션 업로드

---
+++ SolutionSourceCode/2292번_브론즈_2_벌집.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            if (N == 1)
+            {
+                Console.WriteLine("1");
+                return;
+            }
+
+            int room_outside_count = 6;
+            int max_room_number = 2;
+            int passing_room_counter = 1;
+            while (max_room_number <= N)
+            {
+                max_room_number += room_outside_count * passing_room_counter;
+                passing_room_counter++;
+            }
+
+            Console.WriteLine(passing_room_counter);
+        }
+    }
+}
